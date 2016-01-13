0f906c7dfac675b43e35d3ecad3aacea27cf1375
JoshuaPoulin <jpoulin87@gmail.com>
2016-01-13 08:57:10 -0700

    fixed an issue

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ var bodyParser = require('body-parser');
 require('dotenv').load({silent: true});
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var port = Number(process.env.PORT || 3000);
 var app = express();
+// var pg = require('pg');
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
